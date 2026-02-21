e5d458406bf9fb4b0049612c3687126d0e860d04
Göktan <goktan1406@gmail.com>
2026-02-21 15:43:11 +0300

    fix endpoint to images api

--- pages/api/transform.js
+++ pages/api/transform.js
@@ -18,8 +18,6 @@ function getPrompt(effect) {
 }
 
 export default async function handler(req, res) {
-  console.log("TRANSFORM GPT-IMAGE-1 ACTIVE");
-
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Only POST allowed" });
   }
